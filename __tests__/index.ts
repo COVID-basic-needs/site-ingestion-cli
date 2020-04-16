b13292bf6dc46a394583785432c67546899c58c4
Anatoliy Zaslavskiy <toli@tolicodes.com>
2020-04-16 16:53:29 +0800

    Parse Flat Data

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,13 +1,15 @@
-import convert from "./";
+import convert from "../";
 
 describe("convert-food-panty-data", () => {
   it("parses a flat file", async () => {
-    const converted = await convert("data/Arizona_Data_Flat.xlsx");
+    const converted = await convert(
+      `${__dirname}/../data/Arizona_Data_Flat.xlsx`
+    );
 
     const firstRow = converted[0];
 
     expect(firstRow).toEqual({
-      siteName: "Victory Worship Center — Bethlehem House Pantry",
+      siteName: "Victory Worship Center —\nBethlehem House Pantry* ",
       siteStreetAddress: "2561 W Ruthrauff Rd",
       siteCity: "Tucson",
       siteState: "AZ",
@@ -21,7 +23,7 @@ describe("convert-food-panty-data", () => {
       Claims: [],
       url: "www.vwcaz.org",
       "Notes (possibly Pre-COVID)":
-        "Food service -— Sat 6-7:30 AM\nLocated in the Fellowship Hall (building)\nof the Ruthrauff Campus.\nLanguages: English, Spanish",
+        "Food service -— Sat 6-7:30 AM\n\nLocated in the Fellowship Hall (building)\nof the Ruthrauff Campus.\nLanguages: English, Spanish",
     });
 
     expect(converted[2].contactEmail).toEqual("srmoffice@srm-hc.org");
--- index.ts
+++ index.ts
@@ -1,5 +1,34 @@
-import readExcelFile from "read-excel-file";
+import xlsx from "node-xlsx";
 
-export default (fileName) => {
-  return readExcelFile(fileName);
+import flatMap from "./fieldMaps/flat";
+
+const convertToJson = (data) => {
+  const headers = data.shift();
+
+  return data.map((row) => {
+    return headers.reduce((out, header, i) => {
+      const val = row[i];
+      out[header] = val ? "" + val : "";
+      return out;
+    }, {});
+  });
+};
+
+// @todo write better definition
+type FieldMap = {
+  [key: string]: any;
+};
+
+export default async (fileName) => {
+  const sheet = xlsx.parse(fileName);
+  const firstSheet = sheet[0].data;
+
+  const data = convertToJson(firstSheet);
+
+  return data.map((row) => {
+    return Object.entries(flatMap as FieldMap).reduce((out, [field, map]) => {
+      out[field] = typeof map === "function" ? map(row) : row[map];
+      return out;
+    }, {});
+  });
 };
