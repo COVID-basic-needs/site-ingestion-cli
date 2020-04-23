de83e0b3380f79a28fc46a951051abd5d2fe5b4c
Jon Chin <jon@sharebyteaching.org>
2020-04-23 16:21:36 -0400

    added support for csv and for FPC map set

--- fieldMaps/getFieldMapForFile.ts
+++ fieldMaps/getFieldMapForFile.ts
@@ -1,8 +1,23 @@
 import xlsx from "node-xlsx";
 import fieldMaps from "./";
+const parse = require('csv-parse/lib/sync')
 
 export default (fileName) => {
-  const sheets = xlsx.parse(fileName);
+    let sheets;
+    switch(true){
+	case fileName.endsWith('.csv'):
+	    sheets = parse(fileName, {
+		columns: false,
+		skip_empty_lines: true
+	    });
+	    break;
+	case fileName.endsWith('.xlsx'):
+	    sheets = xlsx.parse(fileName);
+	    break;
+	default:
+	    break;
+    }
+
 
   return fieldMaps.reduce((out, fieldMap) => {
     if (out) return out;
--- fieldMaps/index.ts
+++ fieldMaps/index.ts
@@ -1,4 +1,5 @@
 import * as flat from "./flat";
 import * as threeSheet from "./three_sheet";
+import * as FPC from "./FPC";
 
-export default [flat, threeSheet];
+export default [FPC, flat, threeSheet];
