ecfb5adf1bf463fd8c39fc4e3e467633914fbab2
Max Stuart <maxastuart@gmail.com>
2020-06-09 05:27:55 -0700

    add email and password in CLI args

--- cli.ts
+++ cli.ts
@@ -13,7 +13,18 @@ const missingFieldMap = 'Please specify an existing .yaml fieldMap file, or a di
 
     let data = [];
     const path = process.argv[2];
+    const email = process.argv[3];
+    const password = process.argv[4];
     const pathType = await fs.stat(path);
+
+    if (!email) {
+        console.error(`You must include your email. format:
+
+    yarn start <yamlFieldMapPath> <email> <password>
+
+If password is blank, will push to TEST Airtable base instead of Main.`);
+        return false;
+    }
 
     if (pathType.isFile() && (path.endsWith('.yaml') || path.endsWith('.yml'))) {
 
@@ -47,29 +58,28 @@ const missingFieldMap = 'Please specify an existing .yaml fieldMap file, or a di
         return;
     }
 
-    console.log(`Done converting, beginning push of ${data.length} records to site ingestion API...`);
+    console.log(`Done converting, beginning push of ${data.length} records to site ingestion API... (lets see if your password worked ;P)`);
 
     // push to site-ingestion-api in batches of 100
     for (var i = 0; i < data.length; i += 100) {
         var endIndex = Math.min(i + 100, data.length);
         var subArr = data.slice(i, endIndex);
-        await uploadToLambda(subArr);
+        await uploadToLambda(subArr, email, password);
     }
 })();
 
 
-async function uploadToLambda(data) {
+async function uploadToLambda(data, email, password) {
     try {
         const { body } = await got.post('https://i3tmnkgp2i.execute-api.us-west-2.amazonaws.com/upload-site', {
-            json: { "data": data },
+            json: { data, email, password },
             responseType: 'json',
         });
         console.log(body);
         return true;
     }
     catch (error) {
-        console.error("Server returned: " + error.response.statusCode);
-        console.error(error.response.body);
+        console.error("Server returned: " + error.response.statusCode + "\n" + JSON.stringify(error.response.body));
         return false;
     }
 }
